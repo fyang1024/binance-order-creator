d35a166bceaedbb659a12838ab00330ccce92341
fyang1024 <fyang1024@gmail.com>
2019-09-18 22:33:04 +1000

    Bug fix

--- src/main/java/fund/threebox/boc/OrderCreator.java
+++ src/main/java/fund/threebox/boc/OrderCreator.java
@@ -11,6 +11,8 @@ import com.binance.api.client.domain.event.AggTradeEvent;
 import com.binance.api.client.domain.event.DepthEvent;
 import com.binance.api.client.domain.event.UserDataUpdateEvent;
 
+import java.math.BigDecimal;
+
 public class OrderCreator {
 
     public static void main(String[] args) throws InterruptedException {
@@ -19,11 +21,11 @@ public class OrderCreator {
         }
         String apiKey = args[0];
         String secret = args[1];
-        String asset = args[2];
+        final String asset = args[2];
         final String symbol = args[3].toLowerCase();
         BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(apiKey, secret);
         final BinanceApiRestClient restClient = factory.newRestClient();
-        Account account = restClient.getAccount();
+        final Account account = restClient.getAccount();
         boolean shouldTryAgain = true;
         while(shouldTryAgain) {
             final String balance = account.getAssetBalance(asset).getFree();
@@ -37,8 +39,12 @@ public class OrderCreator {
                 webSocketClient.onAggTradeEvent(symbol, new BinanceApiCallback<AggTradeEvent>() {
                     public void onResponse(final AggTradeEvent response) {
                         System.out.println(response);
-                        NewOrderResponse newOrderResponse = restClient.newOrder(NewOrder.marketSell(symbol.toUpperCase(), balance));
-                        System.out.println("****** Order created ====> " + newOrderResponse);
+                        String balanceString = account.getAssetBalance(asset).getFree();
+                        BigDecimal balance = new BigDecimal(balanceString);
+                        if (balance.compareTo(BigDecimal.ZERO) > 0) {
+                            NewOrderResponse newOrderResponse = restClient.newOrder(NewOrder.marketSell(symbol.toUpperCase(), balanceString));
+                            System.out.println("****** Order created ====> " + newOrderResponse);
+                        }
                     }
                     public void onFailure(final Throwable cause) {
                         System.err.println("Web socket failed");
